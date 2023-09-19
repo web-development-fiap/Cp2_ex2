b3ac43d39ae6848b0ee77b23c29e7d18753e1d9e
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2023-09-19 17:53:31 -0300

    label nome

--- spa-1tdspf-main/src/components/InserirProduto.jsx
+++ spa-1tdspf-main/src/components/InserirProduto.jsx
@@ -3,30 +3,53 @@ import { ListaProdutos } from './ListaProdutos';
 import styles from './InserirProduto.module.css';
 
 export default function InserirProduto(props) {
-    const [novoProduto, setNovoProduto] = useState({
+  const [novoProduto, setNovoProduto] = useState({
+    nome: '',
+    desc: '',
+    preco: 0,
+    img: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNovoProduto({ ...novoProduto, [name]: value });
+  };
+
+  const adicionarProduto = () => {
+    const produto = {
+      id: ListaProdutos.length + 1,
+      ...novoProduto,
+    };
+
+    ListaProdutos.push(produto);
+
+    setNovoProduto({
       nome: '',
       desc: '',
       preco: 0,
       img: '',
     });
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setNovoProduto({ ...novoProduto, [name]: value });
-    };
-  
-    const adicionarProduto = () => {
-      const produto = {
-        id: ListaProdutos.length + 1,
-        ...novoProduto,
-      };
-  
-      ListaProdutos.push(produto);
-  
-      setNovoProduto({
-        nome: '',
-        desc: '',
-        preco: 0,
-        img: '',
-      });
-    };
+  };
+
+  return (
+    <div className={styles['inserir-produto-container']}>
+      <div>
+        <h2>Inserir Novo Produto</h2>
+        <form>
+          <div>
+            <label htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              name="nome"
+              id="nome"
+              value={novoProduto.nome}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
